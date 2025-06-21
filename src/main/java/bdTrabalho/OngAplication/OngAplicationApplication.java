a165a27abbc60b68b906072da79a8fad6c7d9772
unknown <ribas.marcoseduardo@gmail.com>
2025-06-21 11:34:22 -0300

    Primeiro commit

---
+++ src/main/java/bdTrabalho/OngAplication/OngAplicationApplication.java
@@ -0,0 +1,13 @@
+package bdTrabalho.OngAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bdTrabalho/OngAplication/OngAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package bdTrabalho.OngAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
