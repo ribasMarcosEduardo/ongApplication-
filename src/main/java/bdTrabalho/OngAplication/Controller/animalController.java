f29eb345ee8a145e845503dfaed85220fc2b1a1e
unknown <ribas.marcoseduardo@gmail.com>
2025-06-21 11:55:37 -0300

    Primeiro commit.1

---
+++ src/main/java/bdTrabalho/OngAplication/Controller/animalController.java
@@ -0,0 +1,12 @@
+package bdTrabalho.OngAplication.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/animal")
+public class animalController {
+
+}
--- src/main/java/bdTrabalho/OngAplication/OngAplicationApplication.java
+++ src/main/java/bdTrabalho/OngAplication/OngAplicationApplication.java
@@ -9,5 +9,5 @@ public class OngAplicationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OngAplicationApplication.class, args);
 	}
-// RSASA
+
 }
