d4f4c6f94222068ca855dd46f4a595f1e0721bdb
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-06-22 17:47:21 -0300

    feat: Cria verificação de CPF nulo

--- src/main/java/bdTrabalho/OngAplication/Repository/usuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package bdTrabalho.OngAplication.Repository;
-
-import bdTrabalho.OngAplication.Model.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface usuarioRepository extends JpaRepository<Usuarios, Integer> {
-}
