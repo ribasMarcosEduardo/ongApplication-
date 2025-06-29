ce5b3fca7ecce2a57cce452649f928d819cb923c
unknown <ribas.marcoseduardo@gmail.com>
2025-06-29 20:38:22 -0300

    Ajustes

--- src/main/java/bdTrabalho/OngAplication/validator/UsuarioValidator.java
+++ src/main/java/bdTrabalho/OngAplication/validator/UsuarioValidator.java
@@ -12,9 +12,7 @@ public class UsuarioValidator {
     private final UsuarioRepository usuarioRepository;
 
     public void validarNovoUsuario(UsuarioDTO dto) {
-        // Verificar se o CPF já existe
         if (usuarioRepository.existsByCpf(dto.cpf())) {
-            // Se existir, lança uma exceção que será tratada no Controller
             throw new IllegalArgumentException("O CPF informado já está cadastrado.");
         }
     }
